0b5fb700340b3a538fe78f1443c36efbb6063009
Kir_Antipov <kp.antipov@gmail.com>
2024-10-28 23:45:22 +0300

    Fix config screen registration on Forge. *Sigh*
    No wonder we have Fabric instead of this horrendous thing nowadays.
    How did they plan to create an API for every aspect of Minecraft when they can't even decide how the config registration process should look like?
    I swear, they change it like twice a day.

--- src/forge/src/main/java/dev/kir/cubeswithoutborders/client/forge/CubesWithoutBordersImpl.java
+++ src/forge/src/main/java/dev/kir/cubeswithoutborders/client/forge/CubesWithoutBordersImpl.java
@@ -5,10 +5,10 @@ import dev.kir.cubeswithoutborders.client.config.CubesWithoutBordersConfig;
 import dev.kir.cubeswithoutborders.client.util.forge.ModLoaderUtilImpl;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fmlclient.ConfigGuiHandler;
 
 @OnlyIn(Dist.CLIENT)
 @Mod(ModLoaderUtilImpl.MOD_ID)
@@ -27,12 +27,12 @@ public final class CubesWithoutBordersImpl {
         }
 
         ModLoadingContext.get().registerExtensionPoint(
-            ConfigGuiHandler.ConfigGuiFactory.class,
-            () -> new ConfigGuiHandler.ConfigGuiFactory((__, parent) -> ClothConfigScreen.create(
+            ExtensionPoint.CONFIGGUIFACTORY,
+            () -> (__, parent) -> ClothConfigScreen.create(
                 CubesWithoutBordersConfig.getInstance(),
                 ModLoaderUtilImpl.MOD_ID.replace("_", "-"),
                 parent
-            ))
+            )
         );
     }
 }
